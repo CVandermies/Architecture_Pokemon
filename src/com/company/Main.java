d34242e714708bd665f02d694186db61ba77ce69
Mathias <15154@ecam.be>
2020-11-25 13:23:34 +0100

    Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner myObj = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nWelcome to the Pokemon Arena !");
+            System.out.println("\nPokemon #1");
+            ConcretePokemon pokemon1 = ConcretePokemon.createPokemon();
+            System.out.println("\nPokemon #2");
+            ConcretePokemon pokemon2 = ConcretePokemon.createPokemon();
+
+            System.out.println("\nPokemon #1 => name : " + pokemon1.name + ", type: " + pokemon1.type.name + ", hp: " + pokemon1.hp + ", atk: " + pokemon1.atk + ", def: " + pokemon1.def + ", spd: " + pokemon1.spd);
+            System.out.println("Pokemon #2 => name: " + pokemon2.name + ", type: " + pokemon2.type.name + ", hp: " + pokemon2.hp + ", atk: " + pokemon2.atk + ", def: " + pokemon2.def + ", spd: " + pokemon2.spd);
+
+            System.out.println("\nFight !");
+            System.out.println("\nThe winner is : " + Fight.winner(pokemon1, pokemon2) + "\n");
+        }
     }
 }
