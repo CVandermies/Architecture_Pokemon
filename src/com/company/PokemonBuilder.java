2b24014d509c86c561c39f2b938cae08e31f8884
Mathias <15154@ecam.be>
2020-11-27 13:19:29 +0100

    UI simplified

--- src/com/company/PokemonBuilder.java
+++ src/com/company/PokemonBuilder.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 abstract class PokemonBuilder {
 
-    public static ConcretePokemon createPokemon(){
+    public static ConcretePokemon createPokemon() {
         Scanner myObj = new Scanner(System.in);
         String pokemonName;
         String pokemonType;
@@ -15,10 +15,9 @@ abstract class PokemonBuilder {
 
         System.out.println("Enter Pokemon Name");
         pokemonName = myObj.nextLine();
-
         System.out.println("Enter Pokemon Type");
+        System.out.println("Here are all the types :\nNormal, Fight, Flying, Poison, Ground, Rock, Bug, Ghost, Fire, Water, Grass, Electric, Psychic, Ice, Dragon");
         pokemonType = myObj.nextLine();
-
         System.out.println("Enter Pokemon HP (Health Points)");
         pokemonHp = myObj.nextInt();
         System.out.println("Enter Pokemon Attack");
