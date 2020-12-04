de5a34069119f0735a52c824678c79c5b45099c7
Mathias <15154@ecam.be>
2020-12-04 16:02:29 +0100

    open file 1.2 extract txt in array

--- src/com/company/Pokedex.java
+++ src/com/company/Pokedex.java
@@ -1,9 +1,14 @@
 package com.company;
 
 import java.io.*;
+import java.util.*;
+import java.util.Scanner;
 
 public class Pokedex {
     static String path = ".\\.\\docs\\pokedex.txt";
+
+//    static int[][] pokedex = new int[151][9];
+    static ArrayList<HashMap> pokemon = new ArrayList<>();
 
     static void pokedex() throws Exception {
         File file = new File(path);
@@ -11,7 +16,35 @@ public class Pokedex {
         String st;
 
         while ((st = br.readLine()) != null) {
-            System.out.println(st);
+            HashMap<String, String> poke = new HashMap();
+            String[] words = st.split(" ");
+
+//            for (int i = 0; i < words.length; i++) {
+                poke.put("name", words[0]);
+                poke.put("type1", words[1]);
+                poke.put("type2", words[2]);
+                poke.put("hp", words[3]);
+                poke.put("att", words[4]);
+                poke.put("def", words[5]);
+                poke.put("speed", words[6]);
+//            }
+            pokemon.add(poke);
         }
+        System.out.println(pokemon);
     }
 }
+
+//public class Pokedex {
+//    static String path = ".\\.\\docs\\pokedex.txt";
+//    File file = new File(path);
+//    Scanner in = new Scanner(file);
+//    while(in.hasNextLine()) {
+//        String currentLine = in.nextLine();
+//        String[] words = currentLine.split(" ");
+//        for(String a : words){
+//            if(a.equals("YourWord")) {
+//                // Do something
+//            }
+//        }
+//    }
+//}
